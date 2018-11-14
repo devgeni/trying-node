b298e0ef0c7359ead55144585c5f4b60a1c6df59
Evgeni B <eujeni@list.ru>
2018-11-14 10:06:17 +0300

    Backpressure concept

--- write-stream.js
+++ write-stream.js
@@ -1,15 +1,30 @@
 const { createWriteStream, createReadStream } = require('fs');
 
 const originalFile = createReadStream('./textfile.txt');
-const copyFile = createWriteStream('./copy-file.txt');
+const copyFile = createWriteStream('./copy-file.txt', {
+    // we need wide hose
+    highWaterMark: 1024 * 64
+});
 
 originalFile.on('data', (chunk) => {
-    copyFile.write(String(chunk).split('').reverse().join(''));
+    const more = copyFile.write(
+       String(chunk).split('').reverse().join('')
+    );
+
+    if (!more) {
+       console.log('The hose is full. Pause.');
+       originalFile.pause();
+    }
 });
 
 originalFile.on('error', console.error);
 
 originalFile.on('end', () => copyFile.end());
+
+copyFile.on('drain', () => {
+    console.log('The hose is empty. Resume.');
+    originalFile.resume();
+});
 
 copyFile.on('close', () => {
     process.stdout.write('You copied a textfile. 😈');
