025cb264f93595b1d02f3545f423747fcee7a561
Evgeni B <eujeni@list.ru>
2018-11-29 10:28:06 +0300

    http status code 206 partial content

--- stream-server.js
+++ stream-server.js
@@ -16,8 +16,9 @@ createServer(async (req, res) => {
         start = parseInt(start, 10);
         end = end ? parseInt(end, 10) : size - 1;
 
+        // 206 http status code - Partial Content
         res.writeHead(206, {
-            'Content-Range': `bytes=${start}-${end}/${size}`,
+            'Content-Range': `bytes ${start}-${end}/${size}`,
             'Accept-Ranges': 'bytes',
             'Content-Length': (end - start) + 1,
             'Content-Type': 'video/mp4'
