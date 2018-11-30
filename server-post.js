c7e6044374573185c7e1993b9fa77e8589cf5ce1
Evgeni B <eujeni@list.ru>
2018-11-30 10:23:05 +0300

    Post file

---
+++ server-post.js
@@ -0,0 +1,20 @@
+const { createServer } = require('http');
+const { createWriteStream } = require('fs');
+
+createServer((req, res) => {
+
+    if (req.method === 'POST') {
+        req.pipe(res);
+        req.pipe(createWriteStream('./uploaded-file.noext'))
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+            <form method="POST"
+                action="/" enctype="multipart/form-data">
+                <input type="file" name="file_upload" />
+                <button type="submit">Upload file</button>
+            </form>
+        `);
+    }
+
+}).listen(80, () => console.log('server is http://localhost'));
